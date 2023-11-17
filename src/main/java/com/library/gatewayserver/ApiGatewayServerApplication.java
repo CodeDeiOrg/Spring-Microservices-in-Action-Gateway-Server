5b83fd97efb99f0accf9fcc70ab30ce6701b78f8
andrei-georgian.craciun <andreigeorgiancraciun@yahoo.com>
2023-11-17 20:12:13 +0200

    first commit

---
+++ src/main/java/com/library/gatewayserver/ApiGatewayServerApplication.java
@@ -0,0 +1,15 @@
+package com.library.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Hooks;
+
+@SpringBootApplication
+public class ApiGatewayServerApplication {
+
+	public static void main(String[] args) {
+		Hooks.enableAutomaticContextPropagation();
+		SpringApplication.run(ApiGatewayServerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/library/gatewayserver/filters/ResponseFilter.java
@@ -0,0 +1,40 @@
+package com.library.gatewayserver.filters;
+
+import brave.Span;
+import brave.Tracer;
+import brave.propagation.TraceContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+import java.util.Optional;
+
+@Configuration
+public class ResponseFilter {
+    private final Logger logger = LoggerFactory.getLogger(ResponseFilter.class);
+    private final Tracer tracer;
+
+    public ResponseFilter(Tracer tracer) {
+        this.tracer = tracer;
+    }
+
+    @Bean
+    public GlobalFilter postGlobalFilter() {
+        return (exchange, chain) -> {
+            final String traceId = Optional.ofNullable(tracer.currentSpan())
+                    .map(Span::context)
+                    .map(TraceContext::traceIdString)
+                    .orElse("null");
+
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                logger.debug("Adding the correlation id to the outbound headers. {}", traceId);
+                exchange.getResponse().getHeaders().add(FilterUtils.CORRELATION_ID, traceId);
+                logger.debug("Completing outgoing request for {}.",
+                        exchange.getRequest().getURI());
+            }));
+        };
+    }
+}
